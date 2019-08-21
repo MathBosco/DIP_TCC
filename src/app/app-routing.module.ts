50dfabc561398265c8e71971cdf37a72d4620cf1
MathBosco <matheus98bosco@gmail.com>
2019-08-21 08:05:55 -0300

    comit 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'lg-prof', loadChildren: './lg-prof/lg-prof.module#LgProfPageModule' },  { path: 'lg-alun', loadChildren: './lg-alun/lg-alun.module#LgAlunPageModule' },
+  { path: 'cadas', loadChildren: './cadas/cadas.module#CadasPageModule' },
 
 ];
 
---
+++ src/app/cadas/cadas.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadas',
+  templateUrl: './cadas.page.html',
+  styleUrls: ['./cadas.page.scss'],
+})
+export class CadasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
