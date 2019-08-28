810abea80fd4b684d808324f9762550721bf2aab
MathBosco <matheus98bosco@gmail.com>
2019-08-27 23:00:40 -0300

    commit 10

--- src/app/cadas/cadas.page.ts
+++ src/app/cadas/cadas.page.ts
@@ -1,37 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthProvider } from '../providers/auth';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import {ToastController} from '@ionic/angular';
+
 @Component({
   selector: 'app-cadas',
   templateUrl: './cadas.page.html',
   styleUrls: ['./cadas.page.scss'],
 })
+
 export class CadasPage implements OnInit {
  
-cadastroForm ={
-  email:'',
-  password:'',
-  nome:'',
-  ra: '',
+formGrupo : FormGroup;
+mensagem : string;
   
-}
+  constructor(private formConstrutor: FormBuilder , public toastController : ToastController) {}
 
-
-  constructor(
-    private authProvider: AuthProvider
-  ) { }
-
-  criarNovaConta(){
-    this.authProvider.register(this.cadastroForm)
-    .then((res) =>{
-      let uid = res.user.uid;
-      console.log(res.user.uid);
-      }
-    )}
-    
-        
-    
-
+  
   ngOnInit() {
+    this.formGrupo = this.formConstrutor.group({
+
+      Ra : [''] , 
+      Nome : [''] , 
+      Email : [''] , 
+      EmailConfirm : [''] , 
+      Senha : [''] , 
+      SenhaConfirm : [''] , 
+
+    });
+  }
+  private Cadastrar(){
+
+    const RA = this.formGrupo.get('Ra').value;
+    const Nome = this.formGrupo.get('Nome').value;
+    const Email = this.formGrupo.get('Email').value;
+    const EmailConfirm = this.formGrupo.get('EmailConfirm').value;
+    const Senha = this.formGrupo.get('Senha').value;
+    const SenhaConfirm = this.formGrupo.get('SenhaConfirm').value;
+
+    this.mensagem = "Você Foi Cadastrado com Sucesso, "  + "\n Bem vindo ao DIP " + Nome + "!";
+    this.toastDuracao();
   }
 
-}
+   async toastDuracao(){
+      const toast = await this.toastController.create({
+
+        message: this.mensagem,
+        duration: 5000
+      });
+
+      toast.present();
+    }
+  }
+
+
