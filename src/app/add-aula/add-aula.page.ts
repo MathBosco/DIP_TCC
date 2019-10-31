7de2f5cd247f9f1b447a49257f12a943ae856f68
MathBosco <matheus98bosco@gmail.com>
2019-10-30 23:14:42 -0300

    commit 0.1.8

--- src/app/add-aula/add-aula.page.ts
+++ src/app/add-aula/add-aula.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-add-aula',
   templateUrl: './add-aula.page.html',
--- src/app/aulas-bebe-prof/aulas-bebe-prof.page.ts
+++ src/app/aulas-bebe-prof/aulas-bebe-prof.page.ts
@@ -1,4 +1,10 @@
+import { ArmazenamentoAula } from './../model/armazenamento-aula';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { AulasStorageService } from './../providers/aulas/aulas-storage.service';
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras } from '@angular/router';
+
 
 @Component({
   selector: 'app-aulas-bebe-prof',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aulas-bebe-prof.page.scss'],
 })
 export class AulasBebeProfPage implements OnInit {
+  listaAulas: ArmazenamentoAula[];
 
-  constructor() { }
+  constructor(
+    private AulasStorageService: AulasStorageService,
+    private rota: Router,
+    private toast: ToastController
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+
+  //Atualiza a Lista de Aulas
+  public atualizarListaAulas() {
+    this.AulasStorageService.listarAulas().then(lista => {
+      this.listaAulas = lista;
+    });
   }
 
+  //Roda na Transição de Pagina
+  ionViewDidEnter() {
+    this.atualizarListaAulas();
+  }
+
+
+  //Inserir Contato
+  inserirAulas() {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        operacao: 'inserir'
+      }
+    };
+
+    //Redirecionamento com navigationExtras por parâmetro
+    this.rota.navigate(['add-aula'], navigationExtras);
+  }
 }
